fa904a5cd37522060d00fcd1f6983909a7e28e77
ptrachu <40343@prot.ac.th>
2025-03-11 11:59:16 +0700

    Add files via upload

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,11 @@
+mae = 0
+while mae <= 10:
+    num = 0
+    print("Mae",mae,':',end=' ')
+    while num <= 10:
+        print(mae*num,end=' ')
+        num += 1
+    mae += 1
+    print('')
+input()
+    
