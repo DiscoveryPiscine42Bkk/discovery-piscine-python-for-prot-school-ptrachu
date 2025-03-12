3b7e63bc5d649891e003476a43294f12dbeec7c7
ptrachu <40343@prot.ac.th>
2025-03-12 14:35:59 +0700

    Add files via upload

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,11 @@
+num1 = int(input('first number : '))
+num2 = int(input('second number : '))
+print('thanks!')
+print(num1,'+',num2,'=',num1+num2)
+print(num1,'-',num2,'=',num1-num2)
+if num2 == 0:
+    print("Error: Cannot Divive by Zero")
+else:    
+    print(num1,'/',num2,'=',num1/num2)
+print(num1,'*',num2,'=',num1*num2)
+input()
