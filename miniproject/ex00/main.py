8559b0b486d770db81ef6f27f881624ce4d45086
ptrachu <40343@prot.ac.th>
2025-03-14 12:47:45 +0700

    checkmate

---
+++ miniproject/ex00/main.py
@@ -0,0 +1,17 @@
+
+from checkmate import checkmate
+
+def main():
+    board = """\
+...... 
+......
+..K...
+..A...
+......
+..Q...\
+"""
+    checkmate(board)
+
+if __name__ == "__main__":
+    main()
+
